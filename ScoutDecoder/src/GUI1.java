ee921741b99d59d401ff21532c63be98d1a02be7
Pazu <lightnesshaku@gmail.com>
2017-01-22 22:05:45 -0500

    Commenting

--- ScoutDecoder/src/GUI1.java
+++ ScoutDecoder/src/GUI1.java
@@ -3,6 +3,7 @@
  * --------------------------------------------------------------------------
  * Changelog: 
  * 1/14/2017 - Conversion to 2017 game, pit find data, and bug fixing
+ * 1/16/2017 - UTF-8 BOM watermark workaround
  * --------------------------------------------------------------------------
  * Bugs:
  * 1/14/2017 - Nothing
@@ -19,7 +20,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
+//import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -31,14 +32,28 @@ import javax.swing.JTextField;
 import javax.swing.JRadioButton;
 import javax.swing.SwingConstants;
 
-import org.apache.commons.io.input.BOMInputStream;
+//import org.apache.commons.io.input.BOMInputStream;
+
+/* Database for all scouting done inserted by file or manual input
+ * Takes the file, seperates it into an array and stores it into an arraylist of teams
+ * Saves all the raw data into a text file named data.txt with all the raw data so data is not lost
+ * Sarch feature to access the data in the arraylist of teams
+ */
 
 public class GUI1
 {
+	// Variables 
 	private static String rawData = "";
 	private static ArrayList<Team> Data = new ArrayList<Team>();
+	private JFrame frmMain;
+	private JTextField txtInput;
+	private JTextField txtTeam;
+	private JTextField txtMatch;
+	private JTextField txtData;
+	private static JTextArea txtOut;
+	private JButton btnFind;
 	
-	// Grabs saved data4
+	// Grabs saved data from database file
 	
 	private static void getData()
 	{
@@ -47,38 +62,38 @@ public class GUI1
 			Scanner fScan;
 			try
 			{
-				fScan = new Scanner(new File("data.txt"));
+				fScan = new Scanner(new File("data.txt")); // Grabs file
 				Match nMatch = new Match();
 				Pit nPit = new Pit();
 				rawData += fScan.nextLine();
-				String[] d = rawData.split("]");
-				for (int i = 1; i < d.length; i++)
+				String[] d = rawData.split("]"); // Splits database rawdata into seperate inputs
+				for (int i = 1; i < d.length; i++) // Read all inputs and adds them to the Arraylist of teams
 				{
 					decoded = decode(d[i]);
 					index = Integer.parseInt(decoded.get(0));
 					if (decoded.get(2).equals("true") || decoded.get(2).equals("false")) // Tell apart if it is pit or match
-					{
+					{ // Adding pit data
 						nPit.setData(decoded);
-						if (Data.size() == 0 || Data.get(index) == null)
+						if (Data.size() == 0 || Data.get(index) == null) // Creates a new team if there is no team/no data
 						{
 							Team nTeam = new Team();
 							nTeam.addPit(nPit);
 							Data.set(index, nTeam);
-						} else
+						} else // Adds data to existing teams
 						{
 							Team tTeam = Data.get(index);
 							tTeam.addPit(nPit);
 							Data.set(index, tTeam);
 						}	
 					} else
-					{
+					{ // Adding match data
 						nMatch.setData(decoded);
-						if (Data.size() == 0 || Data.get(index) == null)
+						if (Data.size() == 0 || Data.get(index) == null) // Creates a new team if there is no team/no data
 						{
 							Team nTeam = new Team();
 							nTeam.addMatch(nMatch);
 							Data.set(index, nTeam);
-						} else
+						} else // Adds data to existing teams
 						{
 							Team tTeam = Data.get(index);
 							tTeam.addMatch(nMatch);
@@ -92,7 +107,7 @@ public class GUI1
 			//{}
 			catch (Exception error)
 			{}
-			outData();
+			outData(); // Saves the file
 		}
 
 	// Decodes the input
@@ -100,29 +115,31 @@ public class GUI1
 	{
 		String[] decode = in.split("}");
 		ArrayList<String> decoded = new ArrayList<String>();
-		for (int i = 0; i < decode.length; i++)
+		for (int i = 0; i < decode.length; i++) // Creates an arraylist from the string
 		{
 			decoded.add(decode[i]);
 		}
 		return decoded;
 	}
 
-	// Saves the data
+	// Saves the data into the file
+	// Rawdata on first line
+	// Organized data on rest
 	private static void outData()
 	{
 		PrintWriter writer;
 		try
 		{
-			writer = new PrintWriter("data.txt");
-			writer.println(rawData);
+			writer = new PrintWriter("data.txt"); // Creates new file
+			writer.println(rawData); // sets the first line to all the inputs for later usage
 			for (int i = 0; i < Data.size(); i++)
 			{
-				if (Data.get(i) != null)
-				{
-					writer.println("Team: " + i);
+				if (Data.get(i) != null) // Ignores all teams without data
+				{ // Writes out all the data to the file
+					writer.println("Team: " + i); 
 					for (int j = 0; j < Data.get(i).MatchSize(); j++)
 					{
-						Match curr = Data.get(i).getMatch(j);
+						Match curr = Data.get(i).getMatch(j); // Grabs the requested team and match
 						writer.println("\tMatch: " + (j + 1));
 						writer.println("\t\tAuto Gear: " + curr.getAutoGear());
 						writer.println("\t\tAuto Gear Position: " + curr.getAutoGearPos());
@@ -146,7 +163,7 @@ public class GUI1
 					}
 					for (int j = 0; j < Data.get(i).PitSize(); j++)
 					{
-						Pit curr = Data.get(i).getPit(j);
+						Pit curr = Data.get(i).getPit(j); // Grabs the requested team and pit
 						writer.println("\tPit: " + (j + 1));
 						writer.println("\t\tAuto Gear: " + curr.getAutoGear());
 						writer.println("\t\tAuto Baseline: " + curr.getAutoHigh());
@@ -172,20 +189,15 @@ public class GUI1
 			}
 			writer.close();
 		} catch (FileNotFoundException e)
-		{
+		{ // If the file fails to save
 			txtOut.setText("Error writing to file");
 		}
 	}
 
 	// GUI Stuff
-	private JFrame frmMain;
-	private JTextField txtInput;
-	private JTextField txtTeam;
-	private JTextField txtMatch;
-	private JTextField txtData;
-	private static JTextArea txtOut;
-	private JButton btnFind;
-
+	// Most of it is unimportant
+	
+	// Starts the GUI
 	public static void main(String[] args)
 	{
 		EventQueue.invokeLater(new Runnable()
@@ -204,21 +216,24 @@ public class GUI1
 			}
 		});
 	}
-
 	public GUI1()
 	{
 		initialize();
 	}
+	
+	// When program closes, it saves to file before closing just in case
 	private int close()
 	{
 		outData();
 		return JFrame.EXIT_ON_CLOSE;
 	}
+	
+	// When the app starts
 	private void initialize()
 	{
-		for (int i = 0; i < 10000; i++)
+		for (int i = 0; i < 10000; i++) // Fills the arraylist full of nulls to give it size
 			Data.add(null);
-		getData();
+		getData(); // Grabs the data file and adds it to the team arraylist
 		frmMain = new JFrame();
 		frmMain.setTitle("Scouting Decoder");
 		frmMain.setResizable(false);
@@ -233,7 +248,7 @@ public class GUI1
 				ArrayList<String> decoded = new ArrayList<String>();
 				String input;
 				int index;
-				input = txtInput.getText();
+				input = txtInput.getText(); // Grabs input text from top
 				try
 				{
 					Match nMatch = new Match();
@@ -243,24 +258,24 @@ public class GUI1
 					if (decoded.get(2).equals("true") || decoded.get(2).equals("false")) // Tell apart if it is pit or match
 					{
 						nPit.setData(decoded);
-						rawData += "]" + input;
-						if (Data.size() == 0 || Data.get(index) == null)
-						{
+						rawData += "]" + input; // Adds input data to rawData
+						if (Data.size() == 0 || Data.get(index) == null) // Creates new team if there is no team/no data
+						{ // Adding pit data
 							Team nTeam = new Team();
 							nTeam.addPit(nPit);
 							Data.set(index, nTeam);
-						} else
-						{
+						} else // Adds data to existing teams
+						{ 
 							Team tTeam = Data.get(index);
 							tTeam.addPit(nPit);
 							Data.set(index, tTeam);
 						}
 						txtOut.setText("Added Pit Data");
 					} else
-					{
+					{ // Adding match data
 						nMatch.setData(decoded);
 						rawData += "]" + input;
-						if (Data.size() == 0 || Data.get(index) == null)
+						if (Data.size() == 0 || Data.get(index) == null) // Creates new team if there is no team/no data
 						{
 							Team nTeam = new Team();
 							nTeam.addMatch(nMatch);
@@ -274,7 +289,7 @@ public class GUI1
 						txtOut.setText("Added Match Data");
 					}
 				}
-				catch (Exception error)
+				catch (Exception error) // Wrong input
 				{
 					txtOut.setText("error");
 				}
@@ -289,43 +304,39 @@ public class GUI1
 		// Exit button that also saves
 		JButton btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener() {
-	public void actionPerformed(ActionEvent e)
-	{
-		outData();
-		frmMain.dispose();
-	}});
+		public void actionPerformed(ActionEvent e)
+		{
+			outData(); // Saves to data.txt
+			frmMain.dispose();
+		}});
 		btnExit.setBounds(12,777,97,25);
 		frmMain.getContentPane().add(btnExit);
 		
+		// Unimportant GUI stuff
 		txtInput = new JTextField();
 		txtInput.setToolTipText("Input");
 		txtInput.setBounds(12,13,1104,49);
 		frmMain.getContentPane().add(txtInput);
 		txtInput.setColumns(10);
-
 		txtOut = new JTextArea();
 		txtOut.setEditable(false);
 		txtOut.setBounds(24,137,1104,539);
 		frmMain.getContentPane().add(txtOut);
-
 		txtTeam = new JTextField();
 		txtTeam.setToolTipText("Insert Team # In Here");
 		txtTeam.setBounds(856,704,116,22);
 		frmMain.getContentPane().add(txtTeam);
 		txtTeam.setColumns(10);
-
 		txtMatch = new JTextField();
 		txtMatch.setToolTipText("Insert Match");
 		txtMatch.setBounds(856,739,116,22);
 		frmMain.getContentPane().add(txtMatch);
 		txtMatch.setColumns(10);
-
 		txtData = new JTextField();
 		txtData.setToolTipText("Insert which data you want (add \"avg\" or type \"all\" for all data)");
 		txtData.setBounds(856,778,116,22);
 		frmMain.getContentPane().add(txtData);
 		txtData.setColumns(10);
-
 		JRadioButton btnPit = new JRadioButton("Pit Data");
 		btnPit.setHorizontalAlignment(SwingConstants.TRAILING);
 		btnPit.setBounds(694, 738, 127, 25);
@@ -336,12 +347,12 @@ public class GUI1
 		btnFind.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
 			try {
-				if (btnPit.isSelected())
+				if (btnPit.isSelected()) // If pit radio button is selected, user is looking for pit data instead of match
 				{
 					int teamI = Integer.parseInt(txtTeam.getText());
 					int pitI = Integer.parseInt(txtMatch.getText());
 					Team team = Data.get(teamI);
-					Pit curr = team.getPit(pitI - 1);
+					Pit curr = team.getPit(pitI - 1); // Grabs the requested team and pit
 					txtOut.setText(txtOut.getText() + "\nAuto Gear: " + curr.getAutoGear());
 					txtOut.setText(txtOut.getText() + "\nAuto High Goal: " + curr.getAutoHigh());
 					txtOut.setText(txtOut.getText() + "\nAuto Low Goal: " + curr.getAutoLow());
@@ -361,12 +372,13 @@ public class GUI1
 					txtOut.setText(txtOut.getText() + "\nField Usage: " + curr.getFieldUsage());
 					txtOut.setText(txtOut.getText() + "\nScouter: " + curr.getScouter());
 					txtOut.setText(txtOut.getText() + "\nComments: " + curr.getComment());
-				} else {
+				} else 
+				{ // Else user is looking for match data, outputs match data
 					int teamI = Integer.parseInt(txtTeam.getText());
 					int matchI = Integer.parseInt(txtMatch.getText());
 					String dataI = txtData.getText();
 					Team team = Data.get(teamI);
-					Match curr = team.getMatch(matchI - 1);
+					Match curr = team.getMatch(matchI - 1); // Grabs the requested team and match
 					switch(dataI)
 					{
 						case "":
@@ -443,7 +455,7 @@ public class GUI1
 					}
 				}
 			} catch (Exception error) {txtOut.setText("invalid input");}
-	}});
+		}});
 		btnFind.setBounds(1019,738,97,25);
 		frmMain.getContentPane().add(btnFind);
 	
@@ -451,22 +463,30 @@ public class GUI1
 		JButton btnFindFile = new JButton("Find File");
 		btnFindFile.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e){
+			boolean workaround = true;
 			JFileChooser fc = new JFileChooser();
-			if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+			if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) // Shows user file chooser 
 			{
-				File file = fc.getSelectedFile();
+				File file = fc.getSelectedFile(); // Sets file to the file that the user selected
 				try
 				{
 					Scanner inputFile = new Scanner(file);
-					while(inputFile.hasNext())
+					while(inputFile.hasNext()) // Does until there is no more data
 					{
 						ArrayList<String> decoded = new ArrayList<String>();
 						String input;
 						int index;
-						input = inputFile.next();
-						if(!input.split("}")[2].equals("true") || !input.split("}")[2].equals("false"))
+						input = inputFile.next(); // Grabs string from file
+						if(!input.split("}")[2].equals("true") || !input.split("}")[2].equals("false")) // Accounts for the new line bug error in scouting app 
+							if (workaround)
+							{
+								inputFile.next();
+								workaround = false;
+							}
 							input += inputFile.next();
-						if (input.startsWith("\uFEFF")) {
+							if (inputFile.hasNext())
+								inputFile.next();
+						if (input.startsWith("\uFEFF")) { // Workaround for UTF-8 BOM encoding 
 					        input = input.substring(1);
 					    }
 						System.out.println(input);
@@ -477,15 +497,15 @@ public class GUI1
 							decoded = decode(input);
 							index = Integer.parseInt(decoded.get(0));
 							if (decoded.get(2).equals("true") || decoded.get(2).equals("false")) // Tell apart if it is pit or match
-							{
+							{ // Pit data
 								nPit.setData(decoded);
 								rawData += "]" + input;
-								if (Data.size() == 0 || Data.get(index) == null)
+								if (Data.size() == 0 || Data.get(index) == null) // Creates a new team if there is no team/no data
 								{
 									Team nTeam = new Team();
 									nTeam.addPit(nPit);
 									Data.set(index, nTeam);
-								} else
+								} else // Adds data to existing team
 								{
 									Team tTeam = Data.get(index);
 									tTeam.addPit(nPit);
@@ -493,15 +513,15 @@ public class GUI1
 								}
 								txtOut.setText("Added Pit Data");
 							} else
-							{
+							{ // Match data
 								nMatch.setData(decoded);
 								rawData += "]" + input;
-								if (Data.size() == 0 || Data.get(index) == null)
-								{
+								if (Data.size() == 0 || Data.get(index) == null) // Creates a new team if there is no team/no data
+								{ 
 									Team nTeam = new Team();
 									nTeam.addMatch(nMatch);
 									Data.set(index, nTeam);
-								} else
+								} else // Adds data to existing team
 								{
 									Team tTeam = Data.get(index);
 									tTeam.addMatch(nMatch);
@@ -510,11 +530,10 @@ public class GUI1
 								txtOut.setText("Added Match Data");
 							}
 						}
-						catch (Exception error)
+						catch (Exception error) // File is wrong or formating is wrong
 						{ 
 							txtOut.setText("Invalid Input");
 						}
-					txtInput.setText("");
 					outData();
 					}
 					inputFile.close();
